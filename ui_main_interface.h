a970c67e9463b9f145ea188f7caced7a6c00a537
Abdimk <63399762+abdimk@users.noreply.github.com>
2022-01-16 02:45:08 -0800

    Update ui_main_interface.h

--- ui_main_interface.h
+++ ui_main_interface.h
@@ -1,11 +1,3 @@
-/********************************************************************************
-** Form generated from reading UI file 'main_interfaceQvISPD.ui'
-**
-** Created by: Qt User Interface Compiler version 5.15.2
-**
-** WARNING! All changes made in this file will be lost when recompiling UI file!
-********************************************************************************/
-
 #ifndef MAIN_INTERFACEQVISPD_H
 #define MAIN_INTERFACEQVISPD_H
 
