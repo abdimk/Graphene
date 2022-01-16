097b5159ce332de6f38248785392ef9993cd348d
Abdimk <63399762+abdimk@users.noreply.github.com>
2022-01-16 03:57:19 -0800

    Update ui_main_interface.py

--- ui_main_interface.py
+++ ui_main_interface.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'main_interfaceZkaACu.ui'
-##
-## Created by: Qt User Interface Compiler version 5.15.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
